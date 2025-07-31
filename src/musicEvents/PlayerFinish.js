a533c0f3ac4cd6481f3acfa75101df08832af233
Jean Fourest <jmfourest@gmail.com>
2025-07-31 13:49:52 +0200

    update structure + index.js

--- src/musicEvents/PlayerFinish.js
+++
@@ -1,8 +0,0 @@
-const { GuildQueueEvent } = require("discord-player");
-
-module.exports = {
-  name: GuildQueueEvent.PlayerFinish,
-  execute(queue, tracks) {
-    console.log(`[TRACKS ADDED] ${tracks.length} tracks added`);
-  },
-};
--- src/musicEvents/PlayerStart.js
+++
@@ -1,8 +0,0 @@
-const { GuildQueueEvent } = require("discord-player");
-
-module.exports = {
-  name: GuildQueueEvent.PlayerStart,
-  execute(queue, track) {
-    console.log(`[PLAYER START] Playing: ${track.title}`);
-  },
-};
